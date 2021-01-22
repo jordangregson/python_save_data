5b9352a2ee70516d9b34993c2689c08e53af18b5
Jordan Gregson <49405277+jordangregson@users.noreply.github.com>
2021-01-21 21:57:08 -0500

    updated

--- code.py
+++ code.py
@@ -1,33 +1,8 @@
-import pickle
 import os
-
 os.system("clear")
 
-data = ["Number 1", "Number 2", "Number 3"]
+input = input("Enter text: ")
 
-# Printing the original list
-print("Original List")
-print(data)
-
-# Save the list (wb means to write)
-pickle.dump(data, open("data.dat", "wb"))
-
-# Removing "Number 3"
-print("Removed item List")
-data.remove("Number 3")
-print(data)
-
-#Adding number 4 to the end of the list
-pickle.dump(data, open("data.dat", "wb"))
-print("Added new item List")
-data.append("Number 4")
-print(data)
-
-# Added and Removed another set of numbers
-print("Adding number 5")
-data.append("Number 5")
-print(data)
-
-print("Removing number 5")
-data.remove("Number 5")
-print(data)
+f = open("test2.txt", "w") 
+f.write(input)
+f.close()
